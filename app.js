65a9a9e2e0879f1912950211fbf5781e495cbc54
Florian Porada <florian.porada@gmail.com>
2016-04-08 15:28:55 +0200

    [FEATURE] checkPPN function

--- app.js
+++ app.js
@@ -12,21 +12,38 @@ fs.readFile(path, 'utf8', function (err,data) {
         return console.log(err);
     }
     var lines = data.split('\n');
-    var ids = [];
-    var result = [];
+    var dataSet = [];
 
     for (var i = 0; i < lines.length; i++) {
         var id = lines[i].split(',')[0];
         if (id.length > 3) {
-            ids.push({
-                id: lines[i].split(',')[0],
+            dataSet.push({
+                id: checkPPN(lines[i].split(',')[0]),
                 line: i
             });
-            console.log(lines[i].split(','));
         }
     }
 
     console.log(lines.length);
-    console.log(ids.length);
-    console.log(result);
+    console.log(dataSet);
 });
+
+
+var checkPPN = function(ppn) {
+    if (ppn === undefined || ppn === null) {
+        return 
+    }
+
+    ppn = ppn.toString();
+    if (ppn.length !== 9 && ppn.length < 9) {
+        var l = 9 - ppn.length;
+        var fill = '';
+        for (var i = 0; i < l; i++) {
+            fill += '0';
+        }
+
+        return fill + ppn + '';
+    }
+
+    return ppn;
+};
