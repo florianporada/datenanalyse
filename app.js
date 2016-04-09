404580448f19707d43811fff145b708c0b7d8863
Florian Porada <florian.porada@gmail.com>
2016-04-09 10:06:30 +0200

    [FEATURE] getDuplicates function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ fs = require('fs');
 
 
 var path = 'Liste_PPN-ExNr_HSHN-libre.csv';
+var path2 = 'test.csv';
 var dataSet = [];
 
 fs.readFile(path, 'utf8', function (err,data) {
@@ -13,7 +14,7 @@ fs.readFile(path, 'utf8', function (err,data) {
     }
     var lines = data.split('\n');
 
-    for (var i = 0; i < lines.length; i++) {
+    for (var i = 1; i < lines.length; i++) {
         dataSet.push({
             ppn: parsePPN(lines[i]),
             recordNr: parseRecordNr(lines[i]),
@@ -24,6 +25,7 @@ fs.readFile(path, 'utf8', function (err,data) {
     }
 
     console.log(dataSet);
+    console.log(getDuplicates(dataSet, 'seal'));
 });
 
 var parsePPN = function(line) {
@@ -63,7 +65,7 @@ var parseSignature = function(line) {
     }
     
     //replace PPN + recordNr with emtpy string
-    line = line.replace(line.split(' ')[0] + ' ', '');
+    line = line.replace(line.split(' ')[0], '').trim();
 
     return line.split(',')[0];
 };
@@ -82,4 +84,21 @@ var parseSeal = function(line) {
     }
 
     return line.split(',')[line.split(',').length - 1].replace('\r', '');
+};
+
+var getDuplicates = function(data, key) {
+    var arr = [];
+    var results = [data[1][key]];
+
+    for (var i = 0; i < data.length; i++) {
+        arr.push(data[i][key])
+    }
+
+    for (var j = 0; j < arr.length; j++) {
+        if (results.indexOf(arr[j]) == -1) {
+            results.push(arr[j])
+        }
+    }
+
+    return results;
 };
